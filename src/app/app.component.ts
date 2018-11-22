b2481ca9f4397ab23e11f6fbcf8a10006c15e7ee
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-11-22 11:25:27 -0200

    atualização nos arquivos e configurações nativas do angular para adapta-lo ao projeto desenvolvido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Lara Croft';
-  descricao = 'Lara Croft [Tomb Raider]';
-  url = 'https://i.redd.it/0eyb9ihnrvu01.jpg';
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+/* módulos carregados nativamente pelo angular */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
+/* módulos carregados nativamente pelo angular */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
