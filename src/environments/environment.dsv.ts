b433ffcf9accff6f959ca8856a9a15a4893d6a77
phmiranda <pehhagah.1607@gmail.com>
2020-10-13 15:45:44 -0300

    [add] - criação de configurações de ambiente para produção e desenvolvimento.

---
+++ src/environments/environment.dsv.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiAlbum: 'https://api.dsv.album.phmiranda.com.br',
+  apiKeycloak: 'https://api.dsv.rhsso.phmiranda.com.br',
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiAlbum: 'https://api.album.phmiranda.com.br',
+  apiKeycloak: 'https://api.rhsso.phmiranda.com.br',
 };
